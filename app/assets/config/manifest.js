784f66812052f978d765a9ff7e7bcd26d6a100c8
EmiSuzuki <mush.2525miumiu@gmail.com>
2022-11-16 06:07:17 +0000

    レイアウト編集

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link user/residents.css
+//= link user/registrations.css
+//= link user/sessions.css
+//= link admin/sessions.css
